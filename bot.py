de546bb820e323202c510a7a08a34004b3d83556
Pavel Ermolaev <pikss86@gmail.com>
2020-02-10 19:03:38 +0000

    save

--- bot.py
+++ bot.py
@@ -4,14 +4,6 @@ from telegram.ext import CommandHandler
 
 
 TOKEN = os.environ["SC_TELEGRAM_BOT_TOKEN"]
-REQUEST_KWARGS = {
-    'proxy_url': 'socks5://URL_OF_THE_PROXY_SERVER:PROXY_PORT',
-    # Optional, if you need authentication:
-    #'urllib3_proxy_kwargs': {
-    #    'username': 'PROXY_USER',
-    #    'password': 'PROXY_PASS',
-    }
-}
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
